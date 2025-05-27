db1777a8dcefc8086f1c96b5816631e16cea6dcb
cmo <c.mo@wustl.edu>
2025-05-27 17:13:19 -0500

    update

--- script/DotPlotEnhanced.R
+++ script/DotPlotEnhanced.R
@@ -2,7 +2,7 @@
 # with addition ability to 
 # - Sort identity and feautres by hclust = cluster_gene = T, cluster_sample =T
 # Aug 2022, Simon
-DotPlotEnhanced = function(obj, group.by, features,  title = 'DotPlot', subtitle = '', cluster_gene = T, cluster_sample =T, ...){
+DotPlotEnhanced = function(obj, group.by = 'seurat_clusters', features,  title = 'DotPlot', subtitle = '', cluster_gene = T, cluster_sample =T, ...){
     p = DotPlot(st, group.by=group.by, features = features,...) + 
     labs(title = title, subtitle = subtitle) + 
     scale_color_gradientn(colors = RColorBrewer::brewer.pal(n = 9, name = 'YlOrRd')[2:9])+
